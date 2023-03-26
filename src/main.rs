2200756763c6072471e776c8211222c9da36473e
Eric Feng <erichfeng@gmail.com>
2023-03-25 22:16:15 -0700

    removing column family
    merge_operators not working with column families
    we don't use column families (aside from default)
    so I removed usage of it.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn increment_fn(
     existing_val: Option<&[u8]>,
     operands: &MergeOperands,
 ) -> Option<Vec<u8>> {
-    dbg!(_new_key);
+    // dbg!(&existing_val);
     let mut n = 0;
     if let Some(v) = existing_val {
         n = u32::from_be_bytes(v.try_into().unwrap());
@@ -21,19 +21,17 @@ fn increment_fn(
 
 fn main() {
     let prefix = b"bigarray:";
-    let args: Vec<String> = env::args().collect();
-    let mut options = rocksdb::Options::default();
-    options.create_if_missing(true);
-    options.create_missing_column_families(true);
-    options.set_prefix_extractor(rocksdb::SliceTransform::create_fixed_prefix(prefix.len()));
-    options.set_merge_operator_associative("incr", increment_fn);
-    let default_cf_name = "default";
-    let db = DB::open_cf(&options, "/tmp/rocksdb", &[default_cf_name]).unwrap();
-    let default_cf = db.cf_handle(&default_cf_name).unwrap();
+    let len_key = b"bigarray:len";
 
-    let len_key = b"len";
+    let args: Vec<String> = env::args().collect();
+    let mut opts = rocksdb::Options::default();
+    opts.create_if_missing(true);
+    opts.set_prefix_extractor(rocksdb::SliceTransform::create_fixed_prefix(prefix.len()));
+    opts.set_merge_operator_associative("incr", increment_fn);
+    let db = DB::open(&opts, "/tmp/rocksdb").unwrap();
+
     let n;
-    match db.get_cf(default_cf, len_key) {
+    match db.get(len_key) {
         Ok(Some(value)) => {
             n = u32::from_be_bytes(value.as_slice().try_into().unwrap());
         }
@@ -49,22 +47,24 @@ fn main() {
             panic!("Invalid number of arguments")
         }
         1 => {
-            let mut iterator = db.prefix_iterator_cf(default_cf, prefix);
+            let mut iterator = db.prefix_iterator(prefix);
             while let Some(i) = iterator.next() {
                 let (y, z) = i.unwrap();
                 let k = y.into_vec();
                 let v = String::from_utf8(z.into_vec()).unwrap();
-                if k == b"len" {
+                if k == len_key {
                     continue;
                 }
                 let (_, b) = k.split_at(prefix.len());
-                let a: [u8; 4] = b.try_into().unwrap();
-                let i = i32::from_be_bytes(a);
-                println!("[{}] {}", i, v);
+                if let Ok(c) = b.try_into() {
+                    let a: [u8; 4] = c;
+                    let i = i32::from_be_bytes(a);
+                    println!("[{}] {}", i, v);
+                }
             }
         }
         2 => match args[1].as_str() {
-            "length" => match db.get_cf(default_cf, len_key) {
+            "length" => match db.get(len_key) {
                 Ok(Some(value)) => {
                     let n = u32::from_be_bytes(value.as_slice().try_into().unwrap());
                     println!("{}", n);
@@ -78,10 +78,10 @@ fn main() {
             },
 
             "clear" => {
-                let to_key = [&prefix[..], &n.to_be_bytes()[..]].concat();
-                db.delete_range_cf(&default_cf, &prefix[..], &to_key)
-                    .unwrap();
-                db.delete(len_key).unwrap();
+                let mut batch = rocksdb::WriteBatch::default();
+                let to = [&prefix[..], &[255; 4][..]].concat();
+                batch.delete_range(prefix.to_vec(), to);
+                db.write(batch).unwrap();
             }
             _ => {
                 panic!("Invalid command");
@@ -95,14 +95,14 @@ fn main() {
                         Ok(_) => {
                             let j = n + i as u32 - 2;
                             let key = [&prefix[..], &j.to_be_bytes()[..]].concat();
-                            batch.put_cf(&default_cf, key, &args[i].as_bytes());
+                            batch.put(key, &args[i].as_bytes());
                         }
                         Err(error) => {
                             println!("Error parsing json: {}", error);
                         }
                     }
                 }
-                batch.merge_cf(default_cf, len_key, &(args.len() as u32 - 2).to_be_bytes());
+                batch.merge(len_key, &(args.len() as u32 - 2).to_be_bytes());
                 db.write(batch).unwrap();
                 drop(db);
                 return;
@@ -111,12 +111,12 @@ fn main() {
             "put" => {
                 let i = args[2].parse::<u32>().unwrap();
                 let key = [&prefix[..], &i.to_be_bytes()[..]].concat();
-                db.put_cf(&default_cf, key, &args[3].as_bytes()).unwrap();
+                db.put(key, &args[3].as_bytes()).unwrap();
                 return;
             }
             "get" => {
                 let key = [&prefix[..], &args[2].as_bytes()[..]].concat();
-                let value = db.get_cf(default_cf, key).unwrap();
+                let value = db.get(key).unwrap();
                 match value {
                     Some(value) => {
                         println!("{}", String::from_utf8(value).unwrap());
